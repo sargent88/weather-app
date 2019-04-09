40ee359b8994a487d52d0342628264e9b8633d04
Steen G. Sargent <steen.sargent@gmail.com>
2019-04-09 15:53:46 -0600

    add material, fxflex, normalize.css

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { LayoutComponent } from './layout/layout.component';
 import { AppRoutingModule } from '../app-routing.module';
 
@@ -9,14 +12,16 @@ import { AppRoutingModule } from '../app-routing.module';
   imports: [
     CommonModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   exports: [
     LayoutComponent,
     AppRoutingModule
   ],
   providers: [
-    
+
   ]
 })
 export class CoreModule { }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,14 +1,53 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSlideToggleModule,
+  MatCheckboxModule,
+  MatCardModule,
+  MatDialogModule,
+  MatSnackBarModule,
+  MatIconModule,
+  MatToolbarModule,
+  
+} from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatToolbarModule,
+    
   ],
   exports: [
-
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatToolbarModule,
+    
   ]
 })
 export class MaterialModule { }
