89d7c69f0d671dbcd5ca8a78064f5b271598449e
Steen G. Sargent <steen.sargent@gmail.com>
2019-04-10 14:43:38 -0600

    firebase for db

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  baseurl: '',
+  firebase: {
+    apiKey: "AIzaSyDTBOTdCNVHgt2gxsUBlfpoe8DCp0UZ2vk",
+    authDomain: "weather-app-47dd0.firebaseapp.com",
+    databaseURL: "https://weather-app-47dd0.firebaseio.com",
+    projectId: "weather-app-47dd0",
+    storageBucket: "weather-app-47dd0.appspot.com",
+    messagingSenderId: "557514266893"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: false,
-  baseUrl: ''
+  baseUrl: '',
+  firebase: {
+    apiKey: "AIzaSyDTBOTdCNVHgt2gxsUBlfpoe8DCp0UZ2vk",
+    authDomain: "weather-app-47dd0.firebaseapp.com",
+    databaseURL: "https://weather-app-47dd0.firebaseio.com",
+    projectId: "weather-app-47dd0",
+    storageBucket: "weather-app-47dd0.appspot.com",
+    messagingSenderId: "557514266893"
+  }
 };
 
 /*
