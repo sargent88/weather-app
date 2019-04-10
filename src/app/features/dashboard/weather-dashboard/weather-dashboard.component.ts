fd706fe4d3c16514118b7daca85ea0352da757e5
Steen G. Sargent <steen.sargent@gmail.com>
2019-04-10 13:55:44 -0600

    cards set up

--- src/app/features/dashboard/weather-dashboard/weather-dashboard.component.ts
+++ src/app/features/dashboard/weather-dashboard/weather-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherType } from 'src/app/core/types/weather-type';
+import { CacheService } from './../../../core/services/cache.service';
 
 @Component({
   selector: 'app-weather-dashboard',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-dashboard.component.scss']
 })
 export class WeatherDashboardComponent implements OnInit {
+  weatherData: WeatherType[] = [];
 
-  constructor() { }
+  constructor(
+    private cacheService: CacheService
+  ) { }
 
   ngOnInit() {
+    const weatherObservable = this.cacheService.getWeather();
+    weatherObservable.subscribe((weather: WeatherType[]) => {
+      this.weatherData = weather;
+    })
   }
 
 }
